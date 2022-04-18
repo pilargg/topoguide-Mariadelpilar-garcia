98ed9d711842e0f48ed835bc6977ddeb4e85a709
pilargg <63071055+pilargg@users.noreply.github.com>
2022-04-18 02:35:09 +0200

    ajout des users

--- topoguide/topoguide/settings.py
+++ topoguide/topoguide/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'itineraires.apps.ItinerairesConfig',
+    'itineraires.apps.ItinerairesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
