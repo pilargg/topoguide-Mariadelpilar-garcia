a9a7a0b528a4fd8f6edb3a35281f9f853c18efa0
pilargg <63071055+pilargg@users.noreply.github.com>
2022-04-19 10:35:26 +0200

    ajout des models

--- topoguide/itineraires/admin.py
+++ topoguide/itineraires/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Itineraire
 
 # Register your models here.
+
+admin.site.register(Itineraire)
--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Itineraire(models.Model):
+    titre = models.CharField(max_length=200)
+    pointDepart = models.CharField(max_length=100)
+    description = models.CharField(max_length=350)
+    altitudeDepart = models.FloatField('Altitude de depart (m)')
+    altitudeMin = models.FloatField('Altitude minimale (m)')
+    altitudeMax = models.FloatField('Altitude maximale (m)')
+    denivPosCum = models.FloatField('Dénivelé Positif Cumulé (m)')
+    denivNegCum = models.FloatField('Dénivelé Negatif Cumulé (m)')
+    duree = models.TimeField()
+    difficulte = models.IntegerField(validators=[MinValueValidator(limit_value=1), MaxValueValidator(limit_value=5)])
