cc76807051cb5cc5ed23fb2d844057c968759698
pilargg <63071055+pilargg@users.noreply.github.com>
2022-04-21 11:46:57 +0200

    Creation et modification de sorties

--- topoguide/itineraires/urls.py
+++ topoguide/itineraires/urls.py
@@ -7,5 +7,7 @@ app_name = 'itineraires'
 urlpatterns = [
     path('', views.itineraires, name='itineraires'),
     path('sortie/<int:itineraire_id>/', views.sorties, name='sorties'),
-    # path('sortie/<int:sortie_id>/', views.sortie_details, name='sortie_details'),
+    path('sortie/<int:sortie_id>/', views.sortie, name='sortie_details'),
+    path('sortie/nouvelle_sortie/', views.nouvelle_sortie, name='nouvelle_sortie'),
+    path('sortie/modif_sortie/<int:sortie_id>/', views.modif_sortie, name='modif_sortie'),
 ]
--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from .models import Itineraire, Sortie
+from .forms import SortieForm
 
 # Create your views here.
 
@@ -11,9 +12,31 @@ def itineraires(request):
 
 def sorties(request):
     sorties = get_list_or_404(Sortie)
-    return render(request, 'sorties/sorties.html', {'sorties': sorties})
+    return render(request, 'itineraires/sorties.html', {'sorties': sorties})
 
-# def sortie(request):
-#     sortie_details = get_object_or_404(sortie_details.html, pk=sortie_id)
-#     return render(request, 'sorties/sortie_details.html', {'sortie': sortie})
+def sortie(request):
+    sortie_details = get_object_or_404(sortie_details.html, pk=sortie_id)
+    return render(request, 'itineraires/sortie_details.html', {'sortie': sortie})
+
+def nouvelle_sortie(request):
+    if request.method == 'GET':
+        form = SortieForm()
+    elif request.method == 'POST':
+        form = SortieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('itineraires:sorties')
+    return render(request, 'itineraires/nouvelle_sortie.html', {'form': form})
+
+def modif_sortie(request):
+    # il faut ajouter restriction d'utilisateur
+    sortie = Sortie.objects.get(pk=sortie_id)
+    if request.method == 'GET':
+        form = SortieForm(instance=sortie)
+    elif request.method == 'POST':
+        form = SortieForm(request.POST, instance=sortie)
+        if form.is_valid():
+            form.save()
+            return redirect('itineraires:sorties')
+    return render(request, 'itineraires/modif_sortie.html', {'form': form})
     
