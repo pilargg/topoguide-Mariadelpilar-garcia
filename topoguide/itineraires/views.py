e0c9583ea98183618fe6b45ae15dbbdb6a4fb3dc
pilargg <63071055+pilargg@users.noreply.github.com>
2022-04-22 06:16:13 +0200

    ajout @login_required

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_list_or_404, get_object_or_404, render, redirect
 from django.http import Http404
 
@@ -6,23 +7,23 @@ from .forms import SortieForm
 
 # Create your views here.
 
-# @login_required() 
+@login_required() 
 def itineraires(request):
     itineraires = get_list_or_404(Itineraire)
     return render(request, 'itineraires/itineraires.html', {'itineraires': itineraires})
 
-# @login_required() 
+@login_required() 
 def sorties(request, itineraire_id):
     sorties = get_object_or_404(Sortie, pk=itineraire_id)
     return render(request, 'itineraires/sorties.html', {'sorties': sorties})
 
 
-# @login_required() 
+@login_required() 
 def sortie(request, sortie_id):
     sortie_details = get_object_or_404(sortie_details.html, pk=sortie_id)
     return render(request, 'itineraires/sortie_details.html', {'sortie': sortie})
 
-# @login_required() 
+@login_required() 
 def nouvelle_sortie(request):
     if request.method == 'GET':
         form = SortieForm()
@@ -34,7 +35,7 @@ def nouvelle_sortie(request):
     return render(request, 'itineraires/nouvelle_sortie.html', {'form': form})
 
 
-# @login_required() 
+@login_required() 
 def modif_sortie(request, sortie_id):
     # il faut ajouter restriction d'utilisateur
     sortie = Sortie.objects.get(pk=sortie_id)
