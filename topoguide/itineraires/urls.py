d498174e1ec072726d023f5d60d87a6145a88c25
pilargg <63071055+pilargg@users.noreply.github.com>
2022-04-22 06:07:51 +0200

    modifications sur views et templates : modification et creation

--- topoguide/itineraires/urls.py
+++ topoguide/itineraires/urls.py
@@ -6,8 +6,8 @@ app_name = 'itineraires'
 
 urlpatterns = [
     path('', views.itineraires, name='itineraires'),
-    path('sortie/<int:itineraire_id>/', views.sorties, name='sorties'),
+    path('sorties/<int:itineraire_id>/', views.sorties, name='sorties'),
     path('sortie/<int:sortie_id>/', views.sortie, name='sortie_details'),
-    path('sortie/nouvelle_sortie/', views.nouvelle_sortie, name='nouvelle_sortie'),
-    path('sortie/modif_sortie/<int:sortie_id>/', views.modif_sortie, name='modif_sortie'),
+    path('nouvelle_sortie/', views.nouvelle_sortie, name='nouvelle_sortie'),
+    path('modif_sortie/<int:sortie_id>/', views.modif_sortie, name='modif_sortie'),
 ]
--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,23 +1,28 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render, redirect
+from django.http import Http404
 
 from .models import Itineraire, Sortie
 from .forms import SortieForm
 
 # Create your views here.
 
-
+# @login_required() 
 def itineraires(request):
     itineraires = get_list_or_404(Itineraire)
     return render(request, 'itineraires/itineraires.html', {'itineraires': itineraires})
 
-def sorties(request):
-    sorties = get_list_or_404(Sortie)
+# @login_required() 
+def sorties(request, itineraire_id):
+    sorties = get_object_or_404(Sortie, pk=itineraire_id)
     return render(request, 'itineraires/sorties.html', {'sorties': sorties})
 
-def sortie(request):
+
+# @login_required() 
+def sortie(request, sortie_id):
     sortie_details = get_object_or_404(sortie_details.html, pk=sortie_id)
     return render(request, 'itineraires/sortie_details.html', {'sortie': sortie})
 
+# @login_required() 
 def nouvelle_sortie(request):
     if request.method == 'GET':
         form = SortieForm()
@@ -25,10 +30,12 @@ def nouvelle_sortie(request):
         form = SortieForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('itineraires:sorties')
+            return redirect('itineraires:itineraires')
     return render(request, 'itineraires/nouvelle_sortie.html', {'form': form})
 
-def modif_sortie(request):
+
+# @login_required() 
+def modif_sortie(request, sortie_id):
     # il faut ajouter restriction d'utilisateur
     sortie = Sortie.objects.get(pk=sortie_id)
     if request.method == 'GET':
@@ -37,6 +44,6 @@ def modif_sortie(request):
         form = SortieForm(request.POST, instance=sortie)
         if form.is_valid():
             form.save()
-            return redirect('itineraires:sorties')
+            return redirect('itineraires:itineraires')
     return render(request, 'itineraires/modif_sortie.html', {'form': form})
     
